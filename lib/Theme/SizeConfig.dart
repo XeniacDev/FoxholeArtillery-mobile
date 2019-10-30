7ec8de07e0253d37ce4d76277ff9c1fe3ebd1a77
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-10-30 16:28:20 +0330

    Create SizeConfig class to make the app responsive

---
+++ lib/Theme/SizeConfig.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class SizeConfig {
+  static double _screenWidth;
+  static double _screenHeight;
+  static double _blockSizeHorizontal = 0;
+  static double _blockSizeVertical = 0;
+
+  static double textMultiplier;
+  static double imageSizeMultiplier;
+  static double heightMultiplier;
+
+  void int(BoxConstraints constraints, Orientation orientation) {
+    if (orientation == Orientation.portrait) {
+      _screenWidth = constraints.maxWidth;
+      _screenHeight = constraints.maxHeight;
+    } else {
+      _screenWidth = constraints.maxWidth;
+      _screenHeight = constraints.maxHeight;
+    }
+    _blockSizeHorizontal = _screenWidth / 100;
+    _blockSizeVertical = _screenHeight / 100;
+
+    textMultiplier = _blockSizeVertical;
+    imageSizeMultiplier = _blockSizeHorizontal;
+    heightMultiplier = _blockSizeVertical;
+
+    print('Block Size Vertical: ' + _blockSizeVertical.toString());
+    print('Block Size Horizontal: ' + _blockSizeHorizontal.toString());
+  }
+}
